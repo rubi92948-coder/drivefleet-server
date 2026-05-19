8326e81dfc737fa9085bdbb3e277e4231110a883
rubi92948 <rubi92948@gmail.com>
2026-05-19 22:48:38 +0600

    mongodb connection setup completed

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
 const app = express();
 
+// middleware
 app.use(cors());
 app.use(express.json());
 
+// test route
 app.get("/", (req, res) => {
   res.send("DriveFleet API Running 🚗");
 });
 
-const PORT = 5000;
+// MongoDB connect
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected ✅");
+  })
+  .catch((err) => {
+    console.log("MongoDB Connection Error ❌", err);
+  });
+
+// server listen
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
